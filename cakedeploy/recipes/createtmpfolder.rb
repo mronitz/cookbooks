ebec666aff066d1034e1b2ba927ee8ae2a49e5cb
mronitz <mronitz@gmail.com>
2016-11-04 15:04:37 +0100

    Update createtmpfolder.rb

--- cakedeploy/recipes/createtmpfolder.rb
+++ cakedeploy/recipes/createtmpfolder.rb
@@ -1,13 +1,13 @@
 directory "/srv/www/#{node[:appname]}/current/tmp" do
   mode 0755
-  owner 'www-data'
-  group 'www-data'
+  owner 'apache'
+  group 'apache'
 end
 
 directory "/srv/www/#{node[:appname]}/current/logs" do
   mode 0755
-  owner 'www-data'
-  group 'www-data'
+  owner 'apache'
+  group 'apache'
 end
 
 link "/srv/www/#{node[:appname]}/current/webroot/uploads" do
