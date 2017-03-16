bf988bd9c94869c65d9f2e3cb9ece590b0fb2d76
Maikel Ronitz <mronitz@gmail.com>
2017-03-16 18:17:50 +0100

    Add autoYes

--- php7/recipes/default.rb
+++ php7/recipes/default.rb
@@ -7,7 +7,7 @@ execute 'apt-get update' do
     action :run
 end
 
-execute 'sudo apt-get install php7.0 libapache2-mod-php7.0 php7.0-intl php7.0-mbstring php7.0-curl php-memcache php-memcached php7.0-gd php7.0-gmp php7.0-mcrypt php7.0-mysql php7.0-json' do
+execute 'apt-get -y install php7.0 libapache2-mod-php7.0 php7.0-intl php7.0-mbstring php7.0-curl php-memcache php-memcached php7.0-gd php7.0-gmp php7.0-mcrypt php7.0-mysql php7.0-json' do
     action :run
 end
 
